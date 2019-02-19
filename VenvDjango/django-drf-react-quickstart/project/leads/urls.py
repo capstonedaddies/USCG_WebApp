ae1a05585129c43cc5d36105aa2b9a7fd9848d5a
Sean <shawn.casaus@gmail.com>
2019-02-18 22:14:46 -0700

    Added User authentication with firebase

--- VenvDjango/django-drf-react-quickstart/project/leads/urls.py
+++ VenvDjango/django-drf-react-quickstart/project/leads/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^postsign', views.postsign)
 ]
--- VenvDjango/django-drf-react-quickstart/project/leads/views.py
+++ VenvDjango/django-drf-react-quickstart/project/leads/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect
+import pyrebase
 
 # Create your views here.
 
+config = {
+    'apiKey': "AIzaSyCiXxfNGJ4udL-zC42djLH5yaKMUo5kwEM",
+    'authDomain': "uscg-responder.firebaseapp.com",
+    'databaseURL': "https://uscg-responder.firebaseio.com",
+    'projectId': "uscg-responder",
+    'storageBucket': "uscg-responder.appspot.com",
+    'messagingSenderId': "181260395230"
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
 
 def index(request):
     return render(request, "index.html")
+
+def postsign(request):
+    email = request.POST.get('email')
+    password = request.POST.get("pass")
+
+    user = auth.sign_in_with_email_and_password(email, password)
+
+    return render(request, "welcome.html", {"e":email})
 
 
